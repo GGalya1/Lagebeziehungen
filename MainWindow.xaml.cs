8490f1e1050c6b1713f71460f60e822a52648c5f
student 04 <stk_lum_04@outlook.de>
2022-05-17 13:20:01 +0200

    die Bedingung ist falsch

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -31,9 +32,28 @@ namespace Lagebeziehungen
 
         }
 
-        private void ZahlenEingabePrüfung(object sender, TextCompositionEventArgs e)
+        private void ZahlenEingabePrüfung(object sender, TextCompositionEventArgs e) //ist aus Eigenschaften genommen "PreviewTextInput"
         {
             e.Handled = !Regex.IsMatch(e.Text, "^[+-]?[0-9]*(\\.|\\,)?$");
+            //e.Handled = !Regex.IsMatch(e.Text, "^[+-]?[0-9]*(\\.|\\,)?$");
+            //^ Anfang
+            //$ Ende
+            //| oder
+            //
+            //? 0 oder 1
+            //+ 0 oder beliebig
+            //* 1 oder beliebig (viel Zeichen)
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Vector3 a = new Vector3(); //besteht aus drei Daten mit dem Datentyp "float"
+            Vector3 b = new Vector3();
+            Vector3 c = new Vector3();
+
+            //versuht wird string zu float übersetzen. Wenn gelingt, dann speichern in Variable. Wenn nicht, dann false als rückgabe
+            if ((float.TryParse(tb_ax.Text, out a.X))==false) MessageBox.Show("Eingabefehler in ax");
+             
         }
     }
 }
